5c06f09cd240cc13d471c9ee49131bb09529d927
Ariel Romero <166441368+ArielRomero10@users.noreply.github.com>
2024-11-06 21:24:20 -0300

    feat (Repositories,Controllers)
    -Creation of the new repositories
    -Example YearRepository etc..
    -We do a new  @GetMapping("/getAllYear")

--- src/main/java/com/example/calendarbackend/Controllers/YearController.java
+++ src/main/java/com/example/calendarbackend/Controllers/YearController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/year")
 public class YearController {
-
+    //IMPORTANTE
     @Autowired
     private YearRepository yearRepository;
 
